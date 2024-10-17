2ffd5aeea41e74421f13252fbf3209be995c2475
duyen158 <nguyenthiduyen14062002@gmail.com>
2024-10-17 21:14:12 +0700

    Create_Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThiDuyen158.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
